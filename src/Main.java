7cb0db91c412d337289a2e81f6c4dac805e25fce
FernandoPerera <fernando02perera@gmail.com>
2022-06-15 13:50:31 +0100

    Feature: Creado el menú en el main y el switch, además de haber instanciado la clase Plane y hechos y aplicados los dos primeros métodos, generateF18 y toggleFlaps, además de haber arreglado mejor el toString

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,67 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scanner = new Scanner(System.in);
+
+        Plane plane = new Plane();
+
+        boolean salida = false;
+
+        while (!salida) {
+
+            System.out.println("\n\t==================================");
+            System.out.println("\tHola soldado seleccione una opción");
+            System.out.println("\t==================================\n");
+
+            System.out.println("1 - Inicializar F18");
+            System.out.println("2 - Alternar estado de los flaps ");
+            System.out.println("3 - Alternar estado del tren de aterrizaje");
+            System.out.println("4 - Armar sistema de eyección");
+            System.out.println("5 - Eyectar piloto");
+            System.out.println("Q - Salir");
+
+            String opcion = scanner.nextLine();
+
+            switch (opcion) {
+                case "1":
+                    System.out.println("Soldado introduzca el número de litros de combustible");
+                    float fuel = scanner.nextFloat();
+
+                    System.out.println("Ahora el apodo del piloto");
+                    scanner.nextLine();
+                    String pilotName = scanner.nextLine();
+
+                    System.out.println("Por último el número de escuadron");
+                    String squad = scanner.nextLine();
+
+                    plane.generateF18(fuel, pilotName, squad);
+
+                    System.out.println("\n\t--------------------------");
+                    System.out.println("\tF18 inicializado con exito");
+                    System.out.println("\t--------------------------");
+
+                    break;
+                case "2":
+                    plane.toggleFlaps();
+                    break;
+                case "3":
+
+                    break;
+                case "4":
+
+                    break;
+                case "5":
+
+                    break;
+                case "Q":
+
+                    break;
+                default:
+                    System.out.println("La opción introducida no esta entre las ofrecidas");
+            }
+        }
     }
 }
--- src/Plane.java
+++ src/Plane.java
@@ -4,11 +4,14 @@ public class Plane {
     private String pilotCallSign;
     private String squadNumber;
 
-    private boolean flaps;
-    private boolean landingGear;
-    private boolean ejectionSystem;
+    private boolean flaps = true;
+    private boolean landingGear = false;
+    private boolean ejectionSystem = false;
 
-    private boolean seatOccupation;
+    private boolean seatOccupation = true;
+
+    public Plane() {
+    }
 
     public Plane(float fuelLevel, String pilotCallSign, String squadNumber) {
         this.fuelLevel = fuelLevel;
@@ -18,8 +21,24 @@ public class Plane {
         // Completa el código para cumplir con los requisitos.
     }
 
+    Plane plane = new Plane();
+
+    public void generateF18(float fuel, String pilotName, String squad) {
+
+        Plane avion = new Plane(fuel, pilotName, squad);
+
+        plane = avion;
+
+        System.out.println(plane);
+    }
+
     public void toggleFlaps() {
         flaps = !flaps;
+        if (flaps == true) {
+            System.out.println("Subiendo flaps...");
+        } else {
+            System.out.println("Bajando flaps...");
+        }
     }
 
     public void toggleLandingGear() {
@@ -34,16 +53,44 @@ public class Plane {
         seatOccupation = pilotSeated;
     }
 
+    public String getFlaps() {
+        if (flaps == true) {
+            return "Arriba";
+        } else {
+            return "Abajo";
+        }
+    }
+
+    public String getLandingGear() {
+        if (landingGear == true) {
+            return "Arriba";
+        } else {
+            return "Abajo";
+        }
+    }
+
+    public String getEjectionSystem() {
+        if (ejectionSystem == true) {
+            return "Armado";
+        } else {
+            return "Desarmado";
+        }
+    }
+
+    public String getSeatOccupation() {
+        if (ejectionSystem == true) {
+            return "Verdadero";
+        } else {
+            return "Falso";
+        }
+    }
+
     @Override
     public String toString() {
-        return "Plane{" +
-                "fuelLevel=" + fuelLevel +
-                ", pilotCallSign='" + pilotCallSign + '\'' +
-                ", squadNumber='" + squadNumber + '\'' +
-                ", flaps=" + flaps +
-                ", landingGear=" + landingGear +
-                ", ejectionSystem=" + ejectionSystem +
-                ", seatOccupation=" + seatOccupation +
-                '}';
+        return "\t- - - - Plane - - - -\n" +
+                "\n\t flaps -> " + plane.getFlaps() +
+                "\n\t landingGear -> " + plane.getLandingGear() +
+                "\n\t ejectionSystem -> " + plane.getEjectionSystem() +
+                "\n\t seatOccupation -> " + plane.getSeatOccupation();
     }
 }
